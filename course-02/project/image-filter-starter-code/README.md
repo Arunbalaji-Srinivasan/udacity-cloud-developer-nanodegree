c368b10cfd77aade700ea3150d61c59ec48040ec
Arunbalaji Srinivasan <59905487+Arunbalaji-Srinivasan@users.noreply.github.com>
2020-06-18 19:21:25 -0400

    Added image filter endpoint

--- course-02/project/image-filter-starter-code/README.md
+++ course-02/project/image-filter-starter-code/README.md
@@ -46,3 +46,6 @@ Prevent requests without valid authentication headers.
 
 Add your own domain name and have it point to the running services (try adding a subdomain name to point to the processing server)
 > !NOTE: Domain names are not included in AWS’ free tier and will incur a cost.
+
+### link to the endpoint URL for a running elastic beanstalk deployment
+http://insta-image-gray-filter-api-dev.us-east-2.elasticbeanstalk.com/
--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -10,6 +10,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Set the network port
   const port = process.env.PORT || 8082;
   
+  
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -19,13 +20,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // IT SHOULD
   //    1
   //    1. validate the image_url query
+  app.get('/filteredimage',async (req, res) => {
+    const image_url = req.query.image_url;
+    if (!image_url) {
+      return res.status(400).send({ message: 'Image URL is required or malformed' });
+    }   
   //    2. call filterImageFromURL(image_url) to filter the image
+  const imagePath = await filterImageFromURL(image_url);
   //    3. send the resulting file in the response
+  res.sendFile(imagePath);
   //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  return res.on(imagePath,() => deleteLocalFiles([imagePath]));
+});
 
   /**************************************************************************** */
 
